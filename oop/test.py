f315f6ed6e1bd9024d4be3a6f3a059ce2ae615b8
james <james@chsc.tw>
2022-12-14 13:20:21 +0800

    update

---
+++ oop/test.py
@@ -0,0 +1,21 @@
+def fn():
+    global a
+    print('fn():',a)
+    a = 11
+    print('fn():',a)
+
+def fn1():
+    global a
+    print('fn1():', a)
+    a = 22
+    print('fn1():', a)
+
+class Cls:
+    def __init__(self, a, b) -> None:
+        self.a = a
+        self.b = b
+
+# a = 1
+fn()
+print('main:',a)
+fn1()
