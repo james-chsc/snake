10ba8f028df9324ff7f1ac6609199132551653d0
james <james@chsc.tw>
2022-12-14 13:56:38 +0800

    update

--- oop/snake.py
+++ oop/snake.py
@@ -3,7 +3,7 @@ import random
 
 
 class Point:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int) -> None:
         """
         給定 x 和 y 然後產生一個點
         """
@@ -12,7 +12,7 @@ class Point:
 
 
 class Region:
-    def __init__(self, p1:Point, p2:Point):
+    def __init__(self, p1:Point, p2:Point) -> None:
         """
         給兩個點，定義一個矩形範圍
         """
@@ -24,14 +24,25 @@ class Region:
         產生範圍內的隨機一個「點」
         除了except_points(list of Point)之外
         """
-        return Point( 
-            random.randrange(self.p1.x, self.p2.x), 
-            random.randrange(self.p1.y, self.p2.y) 
-        )
+        while True:
+            pt = Point( 
+                random.randrange(self.p1.x, self.p2.x), 
+                random.randrange(self.p1.y, self.p2.y) 
+            )
+            if not (pt in except_points): break
 
+    def is_in(self, pt: Point) -> bool:
+        """
+        傳入一個點pt，判斷該點是不是超出本Region範圍
+        """
+        if self.p1.x <= pt.x <= self.p2.x and \
+            self.p1.y <= pt.y <= self.p2.y:
+            return True
+        else:
+            return False
 
 class Snake:
-    def __init__(self, region: Region):
+    def __init__(self, region: Region) -> None:
         """
         傳入一個蛇活動的「範圍」及蛇頭的「點」\n
         建立一隻貪食蛇
@@ -59,20 +70,38 @@ class Snake:
         """
         return self.head == self.food
 
-    def is_die(self) -> bool:
+    def is_died(self) -> bool:
         """
         判斷是否死掉：\n
         1.碰到四周牆壁
         2.吃到自己身體
         """
-        # 怎麼死的
-        return False
         
-    def go(self, dir_: str = 'R') -> None:
+        if self.head in self.body[1:] and self.region.is_in(self.head):
+            return True
+        else:
+            return False
+        
+    def go(self, dir_: str = '右') -> None:
         """
-        往目前方向(dir=L、R、U、D)走一步
+        往目前方向(dir=左、右、上、下)走一步
         """
-        # self.head.x +=1    # self.head.x -=1
-        # self.head.y +=1    # self.head.y -=1
-        # if is_eaten(): self.score+=1 加分
-        pass
+        if dir_ == '左':
+            self.head.x +=1
+        elif dir_ == '左':
+            self.head.x -=1
+        elif dir_ == '上':
+            self.head.y -=1
+        else:
+            self.head.y -=1
+
+class Food:
+    def __init__(self, region: Region, snake:Snake) -> None:
+        """
+        給定範圍內的食物
+        """
+        self.region = region
+        (self.x, self.y) = self.region.rnd_point(except_points=snake.body) 
+        
+    def new_food(self):
+        (self.x, self.y) = self.region.rnd_point(except_points=snake.body) 
